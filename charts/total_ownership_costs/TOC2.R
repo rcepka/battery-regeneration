dcf6841504d03cdf8a19e14ee78b54cf8fd2d4dd
rcepka <rcepka@gmail.come>
2022-12-25 15:16:29 +0100

    toc

--- charts/total_ownership_costs/TOC2.R
+++ charts/total_ownership_costs/TOC2.R
@@ -16,10 +16,43 @@ pacman::p_load(
 
 
 
-backup_batt <- tibble(
-  Y.init = rep(6:10, each = 1, times = 4500),
-  Y.prol = rep(1:5, each = 5, times=900),
-  Reg.Costs = rep(1:100, each = 25, times = 9),
-  Batt.Costs = rep( c(100, 125, 150, 175, 200, 250, 300, 400, 500), each = 2500, times = 1)
-
+backup_batt <- tibble (
+  Years.Initial = rep(5:10, each = 1, times = 5000),
+  Years.Prolonged = rep(1:5, each = 6, times=1000),
+  Regeneration.Costs = rep(1:100, each = 30, times = 10),
+  Battery.Costs = rep( c(75, 100, 125, 150, 175, 200, 250, 300, 400, 500), each = 3000, times = 1)
 )
+
+
+backup_batt <- backup_batt %>%
+  mutate(
+    Years.Total = Years.Initial + Years.Prolonged,
+    TOC.y = num(Battery.Costs / Years.Initial, digits = 2),
+    Regeneration.Costs.y = num(Regeneration.Costs / Years.Prolonged, digits = 2, label = "€"),
+    TOC.Regeneration = Battery.Costs + Regeneration.Costs,
+    TOC.Regeneration.y = num(TOC.Regeneration / Years.Total, digits = 2)) %>%
+  relocate(Years.Total, .after = Years.Prolonged) %>%
+  relocate(TOC.y, .after = Battery.Costs) %>%
+  relocate(Regeneration.Costs, .after = TOC.y) %>%
+  relocate(TOC.Regeneration, .after = Regeneration.Costs)
+
+backup_batt <- backup_batt %>%
+  mutate(
+    TOC.savings.y.eur = TOC.y - TOC.Regeneration.y,
+    TOC.savings.y.perc = 1 - TOC.Regeneration.y / TOC.y,
+    Lifetime.Savings = TOC.savings.y.eur * Years.Total
+    )
+
+
+
+
+
+
+ggplot(
+  data = backup_batt,
+  aes(
+    x = Years.Prolonged,
+    y = Reg.Costs
+  )
+) +
+  geom_point()
